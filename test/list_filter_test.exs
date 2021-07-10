6003a46e86d2018801bc92d6ca32b9d234bf8fee
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-10 17:37:58 -0300

    Changes default test for call/1 test when there are odd numbers

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,12 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "when there are odd numbers, returns the quantity" do
+      list = ["1", "2", "3", "lucario", "o9", "1a", "one", "banana"]
+      response = ListFilter.call(list)
+      expected_response = 2
+      assert expected_response == response
+    end
   end
 end
