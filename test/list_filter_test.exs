c354ffc0fb132e28a8461dc2fbf868437662e994
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-10 17:40:35 -0300

    Adds call/1 test when there aren't odd numbers

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -8,5 +8,12 @@ defmodule ListFilterTest do
       expected_response = 2
       assert expected_response == response
     end
+
+    test "when there aren't odd numbers, returns 0" do
+      list = ["kimi", "tokyo", "2", "pringles", "0", "1a", "394", "banana"]
+      response = ListFilter.call(list)
+      expected_response = 0
+      assert expected_response == response
+    end
   end
 end
