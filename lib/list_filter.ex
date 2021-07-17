ae34c245d4cdf88d4a2089b74aa4a4522e6dfaf1
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-17 19:45:25 -0300

    Adds module and call/1 documentation

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,6 +1,20 @@
 defmodule ListFilter do
+  @moduledoc """
+    This module provides a function to count how much odd numbers are in a list of strings
+  """
+
   require Integer
 
+  @doc """
+    This function counts how much odd numbers are in a list of strings
+
+    ## Parameters
+    list: A list of strings
+
+    ## Examples
+    \tiex> ListFilter.call(["a", "1", "2", "3a"])
+    \t1
+  """
   @spec call(list()) :: integer()
   def call(list) do
     list
